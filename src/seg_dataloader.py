14e5fb27d765d971db9e4432453ce1fa24b329bc
Eugenia <evg.martynova@gmail.com>
2021-02-26 15:26:53 +0100

    Create a data loader from specific segmentation dataset, extract masks and images for segmentation in .npy. clean up

--- src/seg_dataloader.py
+++ src/seg_dataloader.py
@@ -1,27 +1,24 @@
 import os
+import numpy as np
 from torch.utils.data import Dataset
 from utils import CineMRISlice
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 
-# data set should not load the data in init function
-# it should prepare prepare the way lo quickly access the data
-# so the idea is to make a simple base class with little functionality and create subclasses for specific functionality
-
-data_path = "../../data/cinemri_mha/rijnstate"
 
 class SegmentationDatasetFull(Dataset):
     def __init__(self,
                  archive_path,
                  images_folder="images",
                  segmentations_folder="cavity_segmentations"):
-        self.archive_path = archive_path
-        self.images_folder = images_folder
-        self.segmentations_folder = segmentations_folder
-
         # Paths
         self.images_path = os.path.join(archive_path, images_folder)
         self.segmentations_path = os.path.join(archive_path, segmentations_folder)
+
+        # Caches
+        self.images = {}
+        self.masks = {}
+
         self.__extract_segmented_slices__()
 
     def __len__(self):
@@ -29,13 +26,22 @@ class SegmentationDatasetFull(Dataset):
 
     def __getitem__(self, idx):
         slice = self.slices[idx]
-        image_path = slice.build_path(self.images_path)
-        segmentation_path = slice.build_path(self.segmentations_path)
 
-        image = sitk.GetArrayFromImage(sitk.ReadImage(image_path))[0]
-        mask = sitk.GetArrayFromImage(sitk.ReadImage(segmentation_path))[0]
+        if slice.file_name in self.images:
+            image = self.images[slice.file_name]
+        else:
+            image_path = slice.build_path(self.images_path)
+            image = sitk.GetArrayFromImage(sitk.ReadImage(image_path))[0]
+            self.images[slice.file_name] = image
 
-        return (image, mask)
+        if slice.file_name in self.masks:
+            mask = self.masks[slice.file_name]
+        else:
+            segmentation_path = slice.build_path(self.segmentations_path)
+            mask = sitk.GetArrayFromImage(sitk.ReadImage(segmentation_path))[0]
+            self.masks[slice.file_name] = mask
+
+        return image, mask
 
     def __extract_segmented_slices__(self):
         # Patient ids
@@ -49,21 +55,80 @@ class SegmentationDatasetFull(Dataset):
 
             for examination_id in examinations:
                 examination_path = os.path.join(patient_segmentations_path, examination_id)
-                slice_ids = [f for f in os.listdir(examination_path) if
+                slices = [f for f in os.listdir(examination_path) if
                              os.path.isfile(os.path.join(examination_path, f))]
 
-                for slice_id in slice_ids:
-                    self.slices.append(CineMRISlice(slice_id, patient_id, examination_id))
+                for slice in slices:
+                    self.slices.append(CineMRISlice(slice, patient_id, examination_id))
+
+
+class SegmentationDataset(Dataset):
+    def __init__(self,
+                 archive_path,
+                 images_folder="images",
+                 segmentations_folder="masks"):
+        # Paths
+        self.images_path = os.path.join(archive_path, images_folder)
+        self.segmentations_path = os.path.join(archive_path, segmentations_folder)
+
+        # Caches
+        self.images = {}
+        self.masks = {}
+
+        self.__extract_segmented_slices__()
+
+    def __len__(self):
+        return len(self.slices)
+
+    def __getitem__(self, idx):
+        slice = self.slices[idx]
+
+        if slice.file_name in self.images:
+            image = self.images[slice.file_name]
+        else:
+            image_path = slice.build_path(self.images_path)
+            image = np.load(image_path)
+            self.images[slice.file_name] = image
+
+        if slice.file_name in self.masks:
+            mask = self.masks[slice.file_name]
+        else:
+            segmentation_path = slice.build_path(self.segmentations_path)
+            mask = np.load(segmentation_path)
+            self.masks[slice.file_name] = mask
+
+        return image, mask
+
+    def __extract_segmented_slices__(self):
+        # Patient ids
+        self.patient_ids = [f.name for f in os.scandir(self.segmentations_path) if f.is_dir()]
+
+        # Now get all examinations for each patient and create an array of Patients
+        self.slices = []
+        for patient_id in self.patient_ids:
+            patient_segmentations_path = os.path.join(self.segmentations_path, patient_id)
+            examinations = [f.name for f in os.scandir(patient_segmentations_path) if f.is_dir()]
+
+            for examination_id in examinations:
+                examination_path = os.path.join(patient_segmentations_path, examination_id)
+                slices = [f for f in os.listdir(examination_path) if f.endswith(".npy")]
+
+                for slice in slices:
+                    self.slices.append(CineMRISlice(slice, patient_id, examination_id))
 
 
 def main():
-    dataset = SegmentationDatasetFull(data_path)
+    archive_path = "../../data/cinemri_mha/rijnstate"
+    subset_path = "../../data/cinemri_mha/segmentation_subset"
+    dataset = SegmentationDataset(subset_path)
+
     for i in range(10):
-        sample = dataset[i]
+        image, mask = dataset[i]
 
         plt.figure()
-        plt.imshow(sample[0], cmap="gray")
-        plt.imshow(sample[1], alpha=0.2)
+        plt.imshow(image, cmap="gray")
+        masked = np.ma.masked_where(mask == 0, mask)
+        plt.imshow(masked, cmap='autumn', alpha=0.2)
         plt.axis('off')
         plt.show()
 
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,43 @@
 import os
+import numpy as np
 import cv2
 import SimpleITK as sitk
+from cinemri.utils import get_patients, fill_contourous_masks
 
-class CineMRISlice():
-    def __init__(self, id, patient_id, examination_id):
-        self.id = id
+
+class CineMRISlice:
+    def __init__(self, file_name, patient_id, examination_id):
+        self.file_name = file_name
         self.patient_id = patient_id
         self.examination_id = examination_id
 
+        self._image = None
+        self._mask = None
+
     def build_path(self, relative_path):
-        return os.path.join(relative_path, self.patient_id, self.examination_id, self.id)
+        return os.path.join(relative_path, self.patient_id, self.examination_id, self.file_name)
+
+
+def get_patients_without_slices(archive_path,
+                                images_folder="images"):
+
+    patients = get_patients(archive_path, images_folder=images_folder)
+    patients_without_slices = []
+    for patient in patients:
+        if len(patient.slices()) == 0:
+            patients_without_slices.append(patient.id)
+
+    return patients_without_slices
+
+
+# TODO: see if there is loss in quality when converting to .png
+def save_frame(source_path, target_path,  slice_file, index=0):
+    image = sitk.GetArrayFromImage(sitk.ReadImage(source_path))[index]
+    image_target_path = os.path.join(target_path, os.path.splitext(slice_file)[0])
+    # Save .npy
+    np.save("{}.npy".format(image_target_path), image)
+    # Save .png
+    cv2.imwrite("{}.png".format(image_target_path), image / image.max() * 255)
 
 
 def extract_segmentation_data(archive_path,
@@ -19,21 +47,13 @@ def extract_segmentation_data(archive_path,
                               target_images_folder="images",
                               target_segmentations_folder="masks"):
 
-    # create target folders
-    try:
-        os.mkdir(destination_path)
-    except FileExistsError:
-        pass
-
     target_images_path = os.path.join(destination_path, target_images_folder)
     target_segmentations_path = os.path.join(destination_path, target_segmentations_folder)
 
+    # create target folders
     try:
+        os.mkdir(destination_path)
         os.mkdir(target_images_path)
-    except FileExistsError:
-        pass
-
-    try:
         os.mkdir(target_segmentations_path)
     except FileExistsError:
         pass
@@ -42,69 +62,57 @@ def extract_segmentation_data(archive_path,
     images_path = os.path.join(archive_path, images_folder)
     segmentations_path = os.path.join(archive_path, segmentations_folder)
 
-    # Patient ids
-    patient_ids = [f.name for f in os.scandir(segmentations_path) if f.is_dir()]
-
+    patients = get_patients(archive_path, images_folder=segmentations_folder)
     # Now get all examinations for each patient and create an array of Patients
-    slices = []
-    for patient_id in patient_ids:
+    for patient in patients:
+        if len(patient.slices()) == 0:
+            continue
+
         # Create patient folder in both images and masks target directories
-        patient_images_path = os.path.join(target_images_path, patient_id)
+        patient_images_path = os.path.join(target_images_path, patient.id)
+        patient_segmentations_path = os.path.join(target_segmentations_path, patient.id)
+
         try:
             os.mkdir(patient_images_path)
-        except FileExistsError:
-            pass
-
-        patient_segmentations_path = os.path.join(target_segmentations_path, patient_id)
-        try:
             os.mkdir(patient_segmentations_path)
         except FileExistsError:
             pass
 
-        # Find all examinations for a patient
-        patient_path = os.path.join(segmentations_path, patient_id)
-        examinations = [f.name for f in os.scandir(patient_path) if f.is_dir()]
+        for (examination_id, slices) in patient.examinations.items():
+            if len(slices) == 0:
+                continue
 
-        for examination_id in examinations:
             # Create examination folder in both images and masks target directories
             examination_images_path = os.path.join(patient_images_path, examination_id)
-            try:
-                os.mkdir(examination_images_path)
-            except FileExistsError:
-                pass
-
             examination_segmentations_path = os.path.join(patient_segmentations_path, examination_id)
             try:
+                os.mkdir(examination_images_path)
                 os.mkdir(examination_segmentations_path)
             except FileExistsError:
                 pass
 
-            # Find all segmented slices
-            examination_path = os.path.join(patient_path, examination_id)
-            slice_ids = [f for f in os.listdir(examination_path) if os.path.isfile(os.path.join(examination_path, f))]
-
-            for slice_id in slice_ids:
-                # TODO: see if there is loss in quality here
-                # Or maybe just save as MHA?
+            for slice in slices:
                 # read an image and extract the first frame
-                scan_path = os.path.join(images_path, patient_id, examination_id, slice_id)
-                image = sitk.GetArrayFromImage(sitk.ReadImage(scan_path))[0]
-                image_target_path = os.path.join(examination_images_path, os.path.splitext(slice_id)[0])
-                cv2.imwrite("{}.png".format(image_target_path), image/image.max() * 255)
+                slice_path = os.path.join(images_path, patient.id, examination_id, slice)
+                save_frame(slice_path, examination_images_path, slice)
 
                 # read a segmentation mask and extract the first frame
-                segmentation_path = os.path.join(segmentations_path, patient_id, examination_id, slice_id)
-                mask = sitk.GetArrayFromImage(sitk.ReadImage(segmentation_path))[0]
-                mask_target_path = os.path.join(examination_segmentations_path, os.path.splitext(slice_id)[0])
-                cv2.imwrite("{}.png".format(mask_target_path), mask*255)
+                segmentation_path = os.path.join(segmentations_path, patient.id, examination_id, slice)
+                save_frame(segmentation_path, examination_segmentations_path, slice)
 
-                slices.append(CineMRISlice(slice_id, patient_id, examination_id))
-    
-    return slices
 
 
 if __name__ == '__main__':
     archive_path = "../../data/cinemri_mha/rijnstate"
-    destination_path = "../../data/cinemri_mha/segmentation_subset"
 
-    extract_segmentation_data(archive_path, destination_path)
+    fill_contourous_masks(archive_path)
+
+    patients_without_slices = get_patients_without_slices(archive_path)
+    print("Patients without slices")
+    print(patients_without_slices)
+
+    patients_without_segmented_slices = get_patients_without_slices(archive_path, images_folder="cavity_segmentations")
+    print("Patients without segmented slices")
+    print(patients_without_segmented_slices)
+
+    #extract_segmentation_data(archive_path, destination_path)
