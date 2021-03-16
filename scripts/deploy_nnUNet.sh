ca6159da48dc94377e54960520a243897bba1823
Eugenia <evg.martynova@gmail.com>
2021-03-16 15:31:14 +0100

    Update docker files and scripts to build and push docker images

--- scripts/deploy_nnUNet.sh
+++ scripts/deploy_nnUNet.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-VERSION=1.01
-TAG=doduo1.umcn.nl/evgenia/standard
+VERSION=1.02
+TAG=doduo1.umcn.nl/evgenia/nnunet
 
 docker build \
+  --build-arg NNUNET_VERSION=1.6.6 \
   -t $TAG:latest \
   -t $TAG:$VERSION \
-  -f /mnt/netcache/pelvis/projects/evgenia/repos/abdomenmrus-cinemri-cavity-segmentation/dockers/standard/Dockerfile \
+  -f /mnt/netcache/pelvis/projects/evgenia/repos/abdomenmrus-cinemri-cavity-segmentation/dockers/nnUNet/Dockerfile \
   /mnt/netcache/pelvis/projects/evgenia/repos/abdomenmrus-cinemri-cavity-segmentation
 
 docker push $TAG:latest
