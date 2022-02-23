fd90d3d84a891d0784c471552b67a86ede1d412f
brambozz <brambo5@live.nl>
2022-02-23 21:25:48 +0100

    Add picai_eval wrapper

--- inference.py
+++ inference.py
@@ -17,6 +17,7 @@ from src.detection_pipeline import (
     evaluate,
 )
 from src.adhesions import AdhesionType, Adhesion, load_annotations, load_predictions
+from src.evaluation import picai_eval
 from pathlib import Path
 import shutil
 import SimpleITK as sitk
@@ -66,6 +67,9 @@ if __name__ == "__main__":
     visceral_slide_dir = Path("/home/bram/data/registration_method/visceral_slide")
     predictions_path = Path(
         "/home/bram/data/registration_method/predictions/predictions.json"
+    )
+    extended_annotations_path = Path(
+        "/home/bram/data/registration_method/extended_annotations.json"
     )
     annotations_path = (
         ARCHIVE_PATH / "metadata" / "bounding_box_annotations_first_frame.json"
@@ -164,7 +168,7 @@ if __name__ == "__main__":
             )
 
     # Detection
-    if True:
+    if False:
         visceral_slides = load_visceral_slides(visceral_slide_dir)
         predictions = {}
         for visceral_slide in tqdm(visceral_slides):
@@ -218,15 +222,75 @@ if __name__ == "__main__":
         with open(predictions_path, "w") as file:
             json.dump(predictions, file)
 
+    # Write reference standard as json
+    if False:
+        visceral_slides = load_visceral_slides(visceral_slide_dir)
+        predictions = {}
+        for sample in tqdm(dataset):
+            patient_id = sample["PatientID"]
+            study_id = sample["StudyInstanceUID"]
+            series_id = sample["SeriesInstanceUID"]
+
+            # Load mask for detection of adhesion type
+            mask_path = (
+                segmentation_result_dir
+                / "merged_masks"
+                / patient_id
+                / study_id
+                / (series_id + ".mha")
+            )
+            mask_sitk = sitk.ReadImage(str(mask_path))
+            mask_np = sitk.GetArrayFromImage(mask_sitk)
+
+            # Get all reference boxes
+            prediction = sample["box"]
+
+            # Assemble for json format
+            prediction_list = []
+            for box in prediction:
+                p = Adhesion(box)
+                conf = 1
+
+                p.assign_type_from_mask(mask_np)
+                if p.type == AdhesionType.unset:
+                    box_type = "unset"
+                if p.type == AdhesionType.pelvis:
+                    box_type = "pelvis"
+                if p.type == AdhesionType.anteriorWall:
+                    box_type = "anterior"
+                if p.type == AdhesionType.inside:
+                    box_type = "inside"
+                prediction_list.append((box, conf, box_type))
+
+            if patient_id not in predictions:
+                predictions[patient_id] = {}
+            if study_id not in predictions[patient_id]:
+                predictions[patient_id][study_id] = {}
+            predictions[patient_id][study_id][series_id] = prediction_list
+
+        with open(extended_annotations_path, "w") as file:
+            json.dump(predictions, file)
+
     # Metrics
     if True:
         # Load predictions
         predictions = load_predictions(predictions_path)
 
         # Load annotations
-        annotations = load_annotations(annotations_path, as_dict=True)
+        annotations = load_predictions(extended_annotations_path)
 
-        evaluate(predictions, annotations, Path("/tmp"))
+        metrics = picai_eval(predictions, annotations, flat=True)
 
-        # Make FROC
-        # Make ROC
+        # Plot FROC
+        plt.figure()
+        plt.xlabel("Mean number of FPs per image")
+        plt.ylabel("Sensitivity")
+        plt.ylim([0, 1])
+        plt.xscale("log")
+        plt.plot(metrics["FP_per_case"], metrics["sensitivity"])
+        plt.show()
+
+        # Plot ROC
+        plt.figure()
+        plt.plot(metrics["fpr"], metrics["tpr"])
+        plt.show()
---
+++ src/evaluation.py
@@ -0,0 +1,125 @@
+"""A wrapper around the prediction/annotation format of this repository
+and picai_eval's froc routine"""
+import numpy as np
+from prostatemr_evaluation.eval import (
+    froc_from_lesion_evaluations,
+    ap_from_lesion_evaluations,
+)
+from sklearn.metrics import roc_curve, auc
+
+
+def evaluate_case(prediction_list, annotation_list, iou_threshold=0.1):
+    """Gather the list of adhesion candidates, and classify in TP/FP/FN.
+
+    Note: this adds 10 to all confidence scores, because picai_eval does
+    not handle negative confidence scores
+
+    Parameters
+    ----------
+    prediction_list: list of (Adhesion, confidence)
+    annotation_list: list of (Adhesion, 1)
+        Reference annotations/ground truth
+
+    Returns
+    -------
+    y_list: list of (is_adhesion, confidence, iou)
+    """
+    y_list = []
+
+    # If no annotations, all predictions are FP
+    if len(annotation_list) == 0:
+        # All predicted bounding boxes are FPs
+        for adhesion, confidence in prediction_list:
+            y_list.append((0, confidence + 10, 0))
+
+        return y_list
+
+    # Otherwise, for each adhesion in annotation_list, try to find match
+
+    # Tracks which predictions has been assigned to a TP
+    hit_predictions_inds = []
+    # Loop over TPs
+    for adhesion, _ in annotation_list:
+        max_iou = 0
+        max_iou_ind = -1
+        # For simplicity for now one predicted bb can correspond to only one TP
+        for ind, (bounding_box, confidence) in enumerate(prediction_list):
+            curr_iou = adhesion.iou(bounding_box)
+            # Do not use predictions that have already been assigned to a TP
+            if curr_iou > max_iou and ind not in hit_predictions_inds:
+                max_iou = curr_iou
+                max_iou_ind = ind
+
+        # If a maximum IoU is greater than the threshold, consider a TP as found
+        if max_iou >= iou_threshold:
+            y_list.append((1, confidence + 10, max_iou))
+            hit_predictions_inds.append(max_iou_ind)
+        # Otherwise add it as a false negative
+        else:
+            y_list.append((1, 0, 0))
+
+    # Predictions that were not matched with a TP are FPs
+    tp_mask = np.full(len(prediction_list), True, dtype=bool)
+    tp_mask[hit_predictions_inds] = False
+    fps_left = np.array(prediction_list)[tp_mask]
+    for fp, confidence in fps_left:
+        y_list.append((0, confidence + 10, 0))
+
+    return y_list
+
+
+def picai_eval(predictions, annotations, iou_threshold=0.001, flat=False):
+    # TODO filter by adhesion type option
+    y_list = []
+    roc_true = {}
+    roc_pred = {}
+    subject_list = []
+    for idx, series_id in enumerate(annotations):
+        prediction = predictions[series_id]
+        annotation = annotations[series_id]
+        y_list_case = evaluate_case(prediction, annotation, iou_threshold)
+
+        roc_true[series_id] = np.max([a[0] for a in y_list_case])
+        roc_pred[series_id] = np.max([a[1] for a in y_list_case])
+        y_list += y_list_case
+        subject_list.append(series_id)
+
+    # Get adhesion-level results
+    sensitivity, FP_per_case, thresholds, num_lesions = froc_from_lesion_evaluations(
+        y_list=y_list, num_patients=len(annotations)
+    )
+
+    # Calculate recall, precision and average precision
+    AP, precision, recall, _ = ap_from_lesion_evaluations(y_list, thresholds=thresholds)
+
+    # Calculate case-level AUROC
+    fpr, tpr, _ = roc_curve(
+        y_true=[roc_true[s] for s in subject_list],
+        y_score=[roc_pred[s] for s in subject_list],
+        pos_label=1,
+    )
+    auc_score = auc(fpr, tpr)
+
+    if flat:
+        # flatten roc_true and roc_pred
+        roc_true_flat = [roc_true[s] for s in subject_list]
+        roc_pred_flat = [roc_pred[s] for s in subject_list]
+
+    metrics = {
+        "FP_per_case": FP_per_case,
+        "sensitivity": sensitivity,
+        "thresholds": thresholds,
+        "num_lesions": num_lesions,
+        "num_patients": len(annotations),
+        "roc_true": (roc_true_flat if flat else roc_true),
+        "roc_pred": (roc_pred_flat if flat else roc_pred),
+        "AP": AP,
+        "precision": precision,
+        "recall": recall,
+        # patient-level predictions
+        "auroc": auc_score,
+        "tpr": tpr,
+        "fpr": fpr,
+    }
+
+    return metrics
